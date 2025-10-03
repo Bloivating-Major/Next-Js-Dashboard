f135485447507d235daca6aacc61b3f8a2ba4c6e
Sambhav <sambhav12wakhariya@gmail.com>
2025-10-03 09:00:24 +0530

    Checked In : ShadCN Navbar Design

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,23 @@
+import { Moon } from "lucide-react";
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+
 const Navbar = () => {
   return (
-    <nav className="">
-        Navbar
+    <nav className="p-4 flex items-center justify-between">
+      {/* Left Side */}
+      collapse button
+      {/* Right Side */}
+      <div className="flex items-center gap-4">
+        <Link href="/">Dashboard</Link>
+        <Moon />
+        <Avatar>
+          <AvatarImage src="https://github.com/Bloivating-Major.png" alt="@shadcn" />
+          <AvatarFallback>BM</AvatarFallback>
+        </Avatar>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
