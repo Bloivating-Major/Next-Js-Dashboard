55d725d7a194fefd51698b992d31b3c43208c7b7
Sambhav <sambhav12wakhariya@gmail.com>
2025-10-03 09:10:24 +0530

    Checked In : ShadCN Dropdown Menu Design

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,14 @@
-import { Moon } from "lucide-react";
+import { LogOut, Moon, Settings, User } from "lucide-react";
 import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 const Navbar = () => {
   return (
@@ -11,10 +19,22 @@ const Navbar = () => {
       <div className="flex items-center gap-4">
         <Link href="/">Dashboard</Link>
         <Moon />
-        <Avatar>
-          <AvatarImage src="https://github.com/Bloivating-Major.png" alt="@shadcn" />
-          <AvatarFallback>BM</AvatarFallback>
-        </Avatar>
+        
+        <DropdownMenu>
+            <DropdownMenuTrigger>
+                <Avatar>
+                    <AvatarImage src="https://github.com/Bloivating-Major.png" alt="@shadcn" />
+                    <AvatarFallback>BM</AvatarFallback>
+                </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent sideOffset={10}>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem><User className="h-[1.2rem] w-[1.2rem] mr-2"/>Profile</DropdownMenuItem>
+            <DropdownMenuItem><Settings className="h-[1.2rem] w-[1.2rem] mr-2"/>Settings</DropdownMenuItem>
+            <DropdownMenuItem variant="destructive"><LogOut className="h-[1.2rem] w-[1.2rem] mr-2"/>Logout</DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </nav>
   );
