4292b0f63022b96f8827b944f816e9d5644d81f1
Sambhav <sambhav12wakhariya@gmail.com>
2025-10-04 09:20:24 +0530

    Checked In : Sidebar Collapsible Groups

--- components/AppSidebar.tsx
+++ components/AppSidebar.tsx
@@ -7,12 +7,14 @@ import {
   User2,
   ChevronUp,
   Plus,
-  Projector
+  Projector,
+  ChevronDown
 } from "lucide-react";
 import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupAction, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarSeparator } from "./ui/sidebar";
 import Link from "next/link";
 import Image from "next/image";
 import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "./ui/dropdown-menu";
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "./ui/collapsible";
 
 const items = [
   {
@@ -61,7 +63,7 @@ const AppSidebar = () => {
       { /** This is Sidebar Content Start*/}
       <SidebarContent> 
 
-
+      {/* Normal Navigations in Sidebar */}
         <SidebarGroup>
           <SidebarGroupLabel>
             Application
@@ -82,6 +84,7 @@ const AppSidebar = () => {
           </SidebarGroupContent>
         </SidebarGroup>
 
+      {/* Actions In Sidebar */}
       <SidebarGroup>
       <SidebarGroupLabel>Projects</SidebarGroupLabel>
       <SidebarGroupAction title="Add Project">
@@ -109,6 +112,39 @@ const AppSidebar = () => {
       </SidebarGroupContent>
     </SidebarGroup>
 
+      {/* Collapsible in Sidebar */}
+      <Collapsible>
+      <SidebarGroup>
+            <SidebarGroupLabel asChild>
+              <CollapsibleTrigger>
+                Collapsable Group
+                <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+              </CollapsibleTrigger>
+            </SidebarGroupLabel>
+            <CollapsibleContent>
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton asChild>
+                      <Link href="/#">
+                        <Projector />
+                        See All Projects
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton asChild>
+                      <Link href="/#">
+                        <Plus />
+                        Add Project
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </CollapsibleContent>
+          </SidebarGroup>
+      </Collapsible>
 
       </SidebarContent>
       { /** This is Sidebar Content end*/}
