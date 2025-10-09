b49b1e42d952cd02214280cd84857d7b9bb20c87
Sambhav <sambhav12wakhariya@gmail.com>
2025-10-09 09:01:45 +0530

    Checked In : User Profile Page Designing

--- app/users/[username]/page.tsx
+++ app/users/[username]/page.tsx
@@ -1,3 +1,4 @@
+import CardList from "@/components/CardList"
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb"
 
 const SingleUserPage = () => {
@@ -18,6 +19,36 @@ const SingleUserPage = () => {
           </BreadcrumbItem>
         </BreadcrumbList>
       </Breadcrumb>
+      {/* CONTAINER */}
+      <div className="mt-4 flex flex-col xl:flex-row gap-8">
+        {/* LEFT  */}
+        <div className="w-full xl:w-1/3 space-y-6">
+          {/* User Badge */}
+          <div className="bg-primary-foreground p-4 rounded-lg">
+            Badge
+          </div>
+          {/* Information  */}
+          <div className="bg-primary-foreground p-4 rounded-lg">
+            Info
+          </div>
+          {/* Card List */}
+          <div className="bg-primary-foreground p-4 rounded-lg">
+            <CardList title="Recent Transactions" />
+          </div>
+        </div>
+        {/* RIGHT  */}
+        <div className="w-full xl:w-2/3 space-y-6">
+        {/* User Card Container */}
+        <div className="bg-primary-foreground p-4 rounded-lg space-y-2">
+          User Card
+        </div>
+
+        {/* Chart Container */}
+        <div className="bg-primary-foreground p-4 rounded-lg">
+          Chart Container
+        </div>
+        </div>
+      </div>
     </div>
   )
 }
