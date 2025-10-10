edad160002be60d9bbc72d52f0978a708008c207
Sambhav <sambhav12wakhariya@gmail.com>
2025-10-10 10:00:45 +0530

    Checked In : Line Chart Component

--- app/users/[username]/page.tsx
+++ app/users/[username]/page.tsx
@@ -1,3 +1,4 @@
+import AppLineChart from "@/components/AppLineChart";
 import CardList from "@/components/CardList";
 import EditUser from "@/components/EditUser";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
@@ -174,7 +175,8 @@ const SingleUserPage = () => {
 
           {/* Chart Container */}
           <div className="bg-primary-foreground p-4 rounded-lg">
-            Chart Container
+            <h1 className="text-xl font-semibold">User Activity</h1>
+            <AppLineChart />
           </div>
         </div>
       </div>
