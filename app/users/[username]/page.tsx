93060718a888c07b5a8d4812510fd95f85fbcf2d
Sambhav <sambhav12wakhariya@gmail.com>
2025-10-10 09:00:45 +0530

    Checked In : Sheets ShadCN Component Added

--- app/users/[username]/page.tsx
+++ app/users/[username]/page.tsx
@@ -1,9 +1,23 @@
-import CardList from "@/components/CardList"
-import { Badge } from "@/components/ui/badge"
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb"
-import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card"
-import { Progress } from "@/components/ui/progress"
-import { BadgeCheck, Candy, Citrus, Shield } from "lucide-react"
+import CardList from "@/components/CardList";
+import EditUser from "@/components/EditUser";
+import { Badge } from "@/components/ui/badge";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
+import { Progress } from "@/components/ui/progress";
+import { Sheet, SheetTrigger } from "@/components/ui/sheet";
+import { BadgeCheck, Candy, Citrus, Shield } from "lucide-react";
 
 const SingleUserPage = () => {
   return (
@@ -92,6 +106,15 @@ const SingleUserPage = () => {
           </div>
           {/* Information  */}
           <div className="bg-primary-foreground p-4 rounded-lg">
+            <div className="flex items-center justify-between">
+              <h1 className="text-xl font-semibold">User Information</h1>
+              <Sheet>
+                <SheetTrigger asChild>
+                  <Button>Edit User</Button>
+                </SheetTrigger>
+                <EditUser />
+              </Sheet>
+            </div>
             <div className="space-y-4 mt-4">
               <div className="flex flex-col gap-2 mb-8">
                 <p className="text-sm text-muted-foreground">
@@ -131,19 +154,19 @@ const SingleUserPage = () => {
         </div>
         {/* RIGHT  */}
         <div className="w-full xl:w-2/3 space-y-6">
-        {/* User Card Container */}
-        <div className="bg-primary-foreground p-4 rounded-lg space-y-2">
-          User Card
-        </div>
+          {/* User Card Container */}
+          <div className="bg-primary-foreground p-4 rounded-lg space-y-2">
+            User Card
+          </div>
 
-        {/* Chart Container */}
-        <div className="bg-primary-foreground p-4 rounded-lg">
-          Chart Container
-        </div>
+          {/* Chart Container */}
+          <div className="bg-primary-foreground p-4 rounded-lg">
+            Chart Container
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SingleUserPage
+export default SingleUserPage;
