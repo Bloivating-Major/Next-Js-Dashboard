874542286545a58fb3d0b5602bfca41a703b3d64
Sambhav <sambhav12wakhariya@gmail.com>
2025-10-11 11:00:45 +0530

    Checked In : Pagination Added

--- app/payments/data-table.tsx
+++ app/payments/data-table.tsx
@@ -5,6 +5,7 @@ import {
   ColumnDef,
   flexRender,
   getCoreRowModel,
+  getPaginationRowModel,
   useReactTable,
 } from "@tanstack/react-table"
 
@@ -16,6 +17,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
+import { DataTablePagination } from "@/components/TablePagination"
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
@@ -30,6 +32,7 @@ export function DataTable<TData, TValue>({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel : getPaginationRowModel()
   })
 
   return (
@@ -76,6 +79,7 @@ export function DataTable<TData, TValue>({
           )}
         </TableBody>
       </Table>
+      <DataTablePagination table={table} />
     </div>
   )
 }
