e359ce4037ce0daf8834a8cd17067209bf75e735
Sambhav <sambhav12wakhariya@gmail.com>
2025-10-12 11:30:45 +0530

    Checked In : Tanstack Table Ui Updated

--- components/TablePagination.tsx
+++ components/TablePagination.tsx
@@ -23,7 +23,7 @@ export function DataTablePagination<TData>({
   table,
 }: DataTablePaginationProps<TData>) {
   return (
-    <div className="flex items-center justify-between px-2">
+    <div className="flex items-center justify-between px-2 flex-col sm:flex-row gap-4 sm:gap-0 py-4 sm:py-0">
       <div className="flex-1 text-sm text-muted-foreground">
         {table.getFilteredSelectedRowModel().rows.length} of{" "}
         {table.getFilteredRowModel().rows.length} row(s) selected.
